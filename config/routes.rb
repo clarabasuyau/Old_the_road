705bbc08064702f374535cf19d044c5429334d7f
clara basuyau <clarabasuyau@gmail.com>
2022-08-22 15:24:09 +0200

    destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   get 'cars/create'
   get 'cars/update'
-  get 'cars/destroy'
+  delete "cars/:id", to: "cars#destroy"
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
