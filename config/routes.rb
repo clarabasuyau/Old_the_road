7e8926d715c44f2869fd65b1dfc35ec1da6d6620
clara basuyau <clarabasuyau@gmail.com>
2022-08-22 16:08:43 +0200

    quick fix

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "pages#home"
-  ressources :cars, only: [:index, :show]
+  resources :cars, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
