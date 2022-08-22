742b867d81ca5b7fcb2c2a8d8059ce223ce0cf2b
Nidal Moughazi <nid.pro95@gmail.com>
2022-08-22 17:52:51 +0200

    controller a voir après

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,33 @@
+class BookingsController < ApplicationController
+  befocre_action :set_car, only: [:new, :create]
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.car = @car
+    if @booking.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_car
+    @car = Car.find(params[:car_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_rent, :ent_rent, :user_id, :car_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,10 +14,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "pages#home"
-  resources :cars, only: [:index, :show]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
+  resources :cars, only: [:index, :show] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: %i[destroy]
 end
