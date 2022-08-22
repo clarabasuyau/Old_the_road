bbbb901b57d327f062aec5482bb1128aedcc224e
Nidal Moughazi <nid.pro95@gmail.com>
2022-08-22 15:24:29 +0200

    controller+valdiates good

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -8,9 +8,18 @@ class CarsController < ApplicationController
   end
 
   def new
+    @car = Car.new
+
   end
 
   def create
+    @car = Car.new(car_params)
+    @car.list = @list
+    if @car.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -20,5 +29,13 @@ class CarsController < ApplicationController
   end
 
   def destroy
+  end
+
+
+  private
+
+  def car_params
+    params.require(:car).permit(:brand_name, :city, :seats, :user_id)
+
   end
 end
