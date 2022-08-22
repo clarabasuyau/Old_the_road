e891db9363a9508d9f8e8566aed6247e3b561ede
clara basuyau <clarabasuyau@gmail.com>
2022-08-22 17:59:34 +0200

    création base de données voiture

--- Gemfile
+++ Gemfile
@@ -7,6 +7,10 @@ ruby "3.1.2"
 gem "rails", "~> 7.0.3", ">= 7.0.3.1"
 
 gem "devise"
+
+# faker pour la database
+gem "faker"
+
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,21 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+user1 = User.create(
+  name: "jean", email: "blabla@gmail.com", phone_number: "097879", password: "1234507"
+)
+user1.save!
+
+puts 'Creating 15 fake cars...'
+15.times do
+  car = Car.create!(
+    brand_name: Faker::Vehicle.manufacture,
+    seats: Faker::Vehicle.engine_size,
+    city: Faker::Address.city,
+    user: user1
+  )
+  car.save!
+end
+puts 'Finished!'
