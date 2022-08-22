0f9f8cda4f1be8302bda443bc5affe5b4366d3dd
Kevin Trinh <kxt.trinh@gmail.com>
2022-08-22 16:15:43 +0200

    edit update method + route

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "pages#home"
-  resources :cars, only: [:index, :show]
+  resources :cars, only: [:index, :show, :edit, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
