6f994cdc322d0c04a63483867c971db83315cc22
Kevin Trinh <kxt.trinh@gmail.com>
2022-08-23 17:44:58 +0200

    seed scrapping + car model + car index

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,26 @@
 #
 # Examples:
 require 'faker'
+require "open-uri"
+require "nokogiri"
+
+
+# Scrapping start -----------------------------
+
+img = []
+i = 0
+
+html = URI.open("https://unsplash.com/s/photos/car").read
+# 1. Parse HTML
+doc = Nokogiri::HTML(html, nil, "utf-8")
+# 2. For the first 15 results
+doc.search(".YVj9w").first(55).each do |ele|
+  img << ele.attr('src') if i > 19 && i != 31
+  i+=1
+end
+
+# Scrapping end -------------------------------
+
 
 Car.destroy_all
 User.destroy_all
@@ -13,6 +33,7 @@ user1 = User.create(
 user1.save!
 
 puts 'Creating 15 fake cars...'
+i = 0
 15.times do
   car = Car.create!(
     brand_name: Faker::Vehicle.manufacture,
@@ -20,9 +41,11 @@ puts 'Creating 15 fake cars...'
     seats: Faker::Vehicle.engine_size,
     city: Faker::Address.city,
     price: rand(50..150).to_s,
+    picture: img[i],
     overview: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     user: user1
   )
+  i += 1
   car.save!
 end
 puts 'Finished!'
