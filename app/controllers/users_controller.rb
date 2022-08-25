5fe3b8ca51bb0680553d38ec8282959bd0abe07f
clara basuyau <clarabasuyau@gmail.com>
2022-08-25 12:16:07 +0200

    profile pic finally ok

--- app/controllers/users_controller.rb
+++
@@ -1,7 +0,0 @@
-class UsersController < ApplicationController
-before_action :authenticate_user!
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :phone_number, :photo)
-  end
-end
