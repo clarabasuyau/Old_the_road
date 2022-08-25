86d3b5bc694458b9703b1bd4c62099cee5913fb9
Kevin Trinh <kxt.trinh@gmail.com>
2022-08-25 11:43:49 +0200

    create car modal v1, cloudinary is missing

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,7 @@ class DashboardsController < ApplicationController
   def dashboard
     @user = current_user
     @cars = @user.cars
+    @car = Car.new
     @past_bookings = @user.bookings.select {|booking| booking.end_rent < Date.today}
     @future_bookings = @user.bookings.select {|booking| booking.end_rent > Date.today}
   end
